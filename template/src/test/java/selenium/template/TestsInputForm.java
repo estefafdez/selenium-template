f31707ea98efa00a7f222e2ade03f0e17e855c35
isabelaliaga <isabel.algai@gmail.com>
2020-10-09 22:44:57 +0200

    Corrección del segundo test

--- template/src/test/java/selenium/template/TestsInputForm.java
+++ template/src/test/java/selenium/template/TestsInputForm.java
@@ -3,6 +3,9 @@ package selenium.template;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By.ById;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -47,15 +50,27 @@ public class TestsInputForm extends TestSetup{
 		WebElement checkboxDemoOption = driver.findElement(By.xpath("//li[@class='tree-branch']/ul/li/a[contains(text(),'Checkbox Demo')]"));
 		checkboxDemoOption.click();
 		
-		WebElement option1Checkbox = driver.findElement(By.xpath("//input[contains(text(),'Option 1')]"));
-		option1Checkbox.click();
+		WebElement option1Checkbox = driver.findElement(By.xpath("//label[contains(., 'Option 1')]/input"));
 		
-		WebElement option3Checkbox = driver.findElement(By.xpath("//input[contains(text(),'Option 3')]"));
-		option3Checkbox.click();
+		// Método para esperar a una acción que tiene que ocurrir, por ejemplo, que un elemento sea visible
+    		
+		WebDriverWait wait = new WebDriverWait(driver, 20); // El tiempo máximo que esperamos es 20 segundos
 		
-		//No funciona
-		Assert.assertTrue(option1Checkbox.getAttribute("checked").equals("true"), "La opción 1 no está seleccionada");
-		Assert.assertTrue(option3Checkbox.getAttribute("checked").equals("true"), "La opción 3 no está seleccionada");
+		wait.until(ExpectedConditions.visibilityOf(option1Checkbox)); // Esperar a que el elemento sea visible
+		
+		// Acción para realizar una acción definida en selenium como, por ejemplo, mover el foco a un punto concreto
+		Actions act = new Actions(driver);
+		
+		// Como un simple click no basta, mover el foco a ese elemento y forzar el click
+		act.moveToElement(option1Checkbox).click().build().perform();
+		
+		WebElement option3Checkbox = driver.findElement(By.xpath("//label[contains(., 'Option 3')]/input"));
+		
+		wait.until(ExpectedConditions.visibilityOf(option3Checkbox));
+		act.moveToElement(option3Checkbox).click().build().perform();
+		
+		Assert.assertTrue(option1Checkbox.isSelected(), "La opción 1 no está seleccionada");
+		Assert.assertTrue(option3Checkbox.isSelected(), "La opción 3 no está seleccionada");
 
 	}
 	
